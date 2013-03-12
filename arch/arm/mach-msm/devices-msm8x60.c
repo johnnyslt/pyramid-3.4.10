78af80364bbfc099f6ddc3f90d43e550cfc2f6c0
helicopter88 <johnlemon.lol@gmail.com>
2013-03-12 16:35:04 +0000

    shooteru-panel: fix undefined references
    Signed-off-by: helicopter88 <johnlemon.lol@gmail.com>

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -458,6 +458,71 @@ struct platform_device *msm_add_gsbi9_uart(void)
 }
 #endif
 
+/* Use GSBI3 QUP for SPI-2 */
+static struct resource gsbi3_qup_spi_resources[] = {
+	{
+		.name   = "spi_base",
+		.start  = MSM_GSBI3_QUP_PHYS,
+		.end    = MSM_GSBI3_QUP_PHYS + SZ_4K - 1,
+		.flags  = IORESOURCE_MEM,
+	},
+	{
+		.name   = "gsbi_base",
+		.start  = MSM_GSBI3_PHYS,
+		.end    = MSM_GSBI3_PHYS + 4 - 1,
+		.flags  = IORESOURCE_MEM,
+	},
+	{
+		.name   = "spi_irq_in",
+		.start  = GSBI3_QUP_IRQ,
+		.end    = GSBI3_QUP_IRQ,
+		.flags  = IORESOURCE_IRQ,
+	},
+	{
+		.name   = "spidm_channels",
+		.start  = 5,
+		.end    = 6,
+		.flags  = IORESOURCE_DMA,
+	},
+	{
+		.name   = "spidm_crci",
+		.start  = 8,
+		.end    = 7,
+		.flags  = IORESOURCE_DMA,
+	},
+	{
+		.name   = "spi_clk",
+		.start  = 44,
+		.end    = 44,
+		.flags  = IORESOURCE_IO,
+	},
+	{
+		.name   = "spi_cs2",
+		.start  = 43,
+		.end    = 43,
+		.flags  = IORESOURCE_IO,
+	},
+	{
+		.name   = "spi_miso",
+		.start  = 42,
+		.end    = 42,
+		.flags  = IORESOURCE_IO,
+	},
+	{
+		.name   = "spi_mosi",
+		.start  = 41,
+		.end    = 41,
+		.flags  = IORESOURCE_IO,
+	},
+};
+
+struct platform_device msm_gsbi3_qup_spi_device = {
+	.name           = "spi_qsd",
+	.id             = 2,
+	.num_resources  = ARRAY_SIZE(gsbi3_qup_spi_resources),
+	.resource       = gsbi3_qup_spi_resources,
+};
+
 static struct resource gsbi3_qup_i2c_resources[] = {
 	{
 		.name	= "qup_phys_addr",
@@ -2796,6 +2861,7 @@ struct platform_device msm8660_rpm_stat_device = {
 	},
 };
 #endif
+
 
 #define SHARED_IMEM_TZ_BASE 0x2a05f720
 static struct resource tzlog_resources[] = {
